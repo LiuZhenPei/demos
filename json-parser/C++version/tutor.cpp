b0cf1d2261e3502843bf5b869b9f619652bdb9d0
LiuZhenPei <2815419522@qq.com>
2017-04-13 21:23:48 +0800

    rapidJSON更全的基础使用方法

--- json-parser/C++version/tutor.cpp
+++ json-parser/C++version/tutor.cpp
@@ -14,7 +14,7 @@ const char *json = "{"
     "\"Null_val\":null,"
     "\"Int_val\":1024,"
     "\"Float_val\":3.1416,"
-    "\"Array_val\":[1,2,3,4,5]"
+    "\"Array_val\":[1,2,3,4]"
 "}";
 
 void basic_usage(const char *data);
@@ -29,9 +29,10 @@ int main(){
 
 
 void basic_usage(const char *data){
-    Document doc;
+    Document doc; 
     //parser json
     doc.Parse(data);
+    cout<<"\n###Query DOM###"<<endl;
     //query object
     assert(doc.IsObject());
     //query string
@@ -42,10 +43,56 @@ void basic_usage(const char *data){
     //query bool
     assert(doc.HasMember("Bool_val"));
     assert(doc["Bool_val"].IsBool());
+    assert(doc["Bool_val"].IsTrue());
     cout<<"Bool_val = ";
     doc["Bool_val"].GetBool()?(puts("True")):(puts("False"));
     //query null
     cout<<"Null_val = ";
     doc["Null_val"].IsNull()?(puts("null")):(puts("?"));
+    //query int
+    assert(doc.HasMember("Int_val"));
+    assert(doc["Int_val"].IsNumber());
+    assert(doc["Int_val"].IsInt());
+    cout<<"Int_val = "<<doc["Int_val"].GetInt()<<endl;
+    //query float
+    assert(doc.HasMember("Float_val"));
+    assert(doc["Float_val"].IsNumber());
+    assert(doc["Float_val"].IsFloat());
+    cout<<"Float_val = "<<doc["Float_val"].GetFloat()<<endl;
+    //query array
+    assert(doc.HasMember("Array_val"));
+    assert(doc["Array_val"].IsArray());
+    const Value &arr = doc["Array_val"];
+    for(SizeType i = 0;i < arr.Size();i++)
+        cout<<"Array_val["<<i+1<<"] = "<<arr[i].GetInt()<<endl;
+    //Query object
+    static const char* kTypeNames[] =
+            { "Null", "False", "True", "Object", "Array", "String", "Number" };
+    Value::ConstMemberIterator iter;
+    for(iter = doc.MemberBegin();iter < doc.MemberEnd();iter++)
+        cout<<"Type of member "<<iter->name.GetString()<<" is "
+            <<kTypeNames[iter->value.GetType()]<<endl;
+
+
+    cout<<"\n###Modify DOM###"<<endl;
+    //modify Str_val
+    doc["Str_val"] = "Modify Str_val";
+    //add value to Array_val
+    Value &a = doc["Array_val"];
+    Document::AllocatorType &allocator = doc.GetAllocator();
+    for(int i = 5;i <= 7;i++)
+        a.PushBack(i,allocator);
+    //add a string to DOM
+    Value Name;
+    char buf[10];
+    int len = sprintf(buf,"%s","Allen");
+    Name.SetString(buf,static_cast<SizeType>(len),doc.GetAllocator());
+    doc.AddMember("Name",Name,doc.GetAllocator());
+    
+    cout<<"\n###Reformat JSON###"<<endl;
+    StringBuffer sb;
+    PrettyWriter<StringBuffer> writer(sb);
+    doc.Accept(writer);
+    puts(sb.GetString());
 
 }
